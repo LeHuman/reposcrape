5faef1f2727c2bb6d14744fee237199b6cc2057f
LeHuman <47549331+LeHuman@users.noreply.github.com>
2025-08-11 22:54:11 -0700

    feat: :zap: More extensive language color capture

--- src/color.rs
+++ src/color.rs
@@ -1,6 +1,36 @@
 use reqwest::get;
 use serde::Deserialize;
-use std::collections::HashMap; // IMPROVE: Serialized size of HashMap vs BTreeMap vs patricia_tree
+use std::collections::HashMap;
+use tracing::debug; // IMPROVE: Serialized size of HashMap vs BTreeMap vs patricia_tree
+
+// IMPROVE: Fuzzy search of strings at usage
+fn add_lang(map: &mut HashMap<String, String>, lang: String, color: String, is_alias: bool) {
+    let lowercase = lang.to_lowercase();
+
+    if is_alias && (map.contains_key(&lang) || map.contains_key(&lowercase)) {
+        debug!("Ignore language alias, actual names take priority {}", lang);
+        return;
+    }
+
+    if let Some(old_color) = map.insert(lang.to_owned(), color.clone()) {
+        if color != old_color {
+            debug!("Got new color for {}: {} -> {}", lang, old_color, color);
+        }
+    }
+
+    if lowercase == lang {
+        return;
+    }
+
+    if let Some(old_color) = map.insert(lowercase.to_owned(), color.clone()) {
+        if color != old_color {
+            debug!(
+                "Got new color for {}: {} -> {}",
+                lowercase, old_color, color
+            );
+        }
+    }
+}
 
 pub async fn fetch_language_colors() -> Result<HashMap<String, String>, Box<dyn std::error::Error>>
 {
@@ -10,6 +40,8 @@ pub async fn fetch_language_colors() -> Result<HashMap<String, String>, Box<dyn 
     #[derive(Debug, Deserialize)]
     struct Language {
         color: Option<String>,
+        aliases: Option<Vec<String>>,
+        extensions: Option<Vec<String>>,
     }
 
     // Parse the YAML content
@@ -17,11 +49,24 @@ pub async fn fetch_language_colors() -> Result<HashMap<String, String>, Box<dyn 
 
     // Create a map of language to color code, filtering out languages without a color
     let mut language_colors = HashMap::new();
+
     for (lang, info) in languages.iter() {
-        if let Some(color) = &info.color {
-            language_colors.insert(lang.clone(), color.clone());
-            language_colors.insert(lang.to_lowercase(), color.clone());
-            language_colors.insert(lang.to_uppercase(), color.clone());
+        let Some(color) = &info.color else {
+            continue;
+        };
+
+        add_lang(&mut language_colors, lang.clone(), color.clone(), false);
+
+        if let Some(extensions) = &info.extensions {
+            for ext in extensions {
+                add_lang(&mut language_colors, ext.clone(), color.clone(), true);
+            }
+        }
+
+        if let Some(aliases) = &info.aliases {
+            for alias in aliases {
+                add_lang(&mut language_colors, alias.clone(), color.clone(), true);
+            }
         }
     }
 
